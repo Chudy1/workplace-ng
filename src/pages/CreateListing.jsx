52215af30ac62db7f4346e67f16777194c990ef3
Chudy1 <98897666+Chudy1@users.noreply.github.com>
2022-04-03 11:59:47 +0100

    Update CreateListing.jsx

--- src/pages/CreateListing.jsx
+++ src/pages/CreateListing.jsx
@@ -98,6 +98,8 @@ function CreateListing() {
               case "running":
                 console.log("Upload is running");
                 break;
+              default:
+                break;
             }
           },
           (error) => {
